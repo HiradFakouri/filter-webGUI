ae035e6cb63e41e9641f3179645dc7f7f474b627
HiradFakouri <hiradfakouri200@gmail.com>
2023-12-21 17:05:48 +0000

    done mostly

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express'); //packages
 const cors = require('cors');
 const path = require('path');
 const { exec } = require('child_process');
+const fs = require('fs');
 
 //init
 const app = express();
@@ -9,6 +10,13 @@ const PORT = 8080;
 
 //extentintions
 app.use(cors());
+app.use(express.json());
+app.use(express.static('output_images'));
+app.use('/output_images', express.static('output_images'));
+
+let ff;
+
+let del_img;
 
 //multer
 const multer = require('multer');
@@ -17,8 +25,9 @@ const storage = multer.diskStorage({
         cb(null, "images")
     },
     filename: (req, file, cb) => {
-        console.log(file)
-        cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname))
+        //console.log(file)
+        ff = file;
+        cb(null, file.originalname)
     }
 });
 
@@ -30,8 +39,56 @@ app.get("/", (req, res) => {
 })
 
 app.post("/uplode", uplode.single("img"), (req, res) => {
+    if (! del_img) {
+        fs.unlink(`./output_images/${del_img}`, (err) => {
+            if (err) {
+                console.error(err)
+            }else {
+                console.log("output file deleted.")
+            }
+        })//./output_images/${img_name}
+        fs.unlink(`./images/${del_img}`, (err) => {
+            if (err) {
+                console.error(err)
+            }else {
+                console.log("file deleted.")
+            }
+        })
+        //./images/${img_name}
+    }
     console.log(req.file)
     res.send("image uploded");
+})
+
+app.post("/filter", (req, res) => {
+    const filter = req.body.filter;
+    console.log(ff.originalname)
+    exec(`py ./filter_program/main.py ${filter} ./images/${ff.originalname} ./output_images/${ff.originalname}`)
+
+
+    del_img = ff.originalname
+    res.end(filter)
+})
+
+app.post("/delete", (req, res) => {
+    del_img = req.body.img_name;
+
+    fs.unlink(`./output_images/${del_img}`, (err) => {
+        if (err) {
+            console.error(err)
+        }else {
+            console.log("output file deleted.")
+        }
+    })//./output_images/${img_name}
+    fs.unlink(`./images/${del_img}`, (err) => {
+        if (err) {
+            console.error(err)
+        }else {
+            console.log("file deleted.")
+        }
+    })
+    //./images/${img_name}
+
 })
 
 app.listen(PORT, () => {
